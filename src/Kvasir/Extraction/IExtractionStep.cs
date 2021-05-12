f96bf70df9520a22c85adbba62f2fbd914d207a1
Justin Millman <jsmillman@msn.com>
2021-05-11 19:50:42 -0500

    Extraction Plan Revamped
    This commit redesigns the Extraction Layer implemented in the previous commit. While the majority of the concepts remain
    intact, the fundamental structure of the classes and interfaces involved has changed. The primary change is a separation
    of concerns between logic for extracting data out of an input entity and logic for operating on that data. At present,
    the former has only one form (reflection), but the new design leaves room for additional mechanisms; the latter is
    agnostic as to how the source object is obtained, and will happily decompoe or transform something regardless of its
    provenance.
    
    Overall, this design is far more flexible, future-proof, and will align better with the upcoming Reconstitution Layer,
    where some of these patterns will reappear.

---
+++ src/Kvasir/Extraction/IExtractionStep.cs
@@ -0,0 +1,55 @@
+﻿using Kvasir.Schema;
+using System;
+using System.Collections.Generic;
+
+namespace Kvasir.Extraction {
+    /// <summary>
+    ///   The interface describing a unit of logic that extracts and potentially decomposes one or more values out of
+    ///   a source object in database-ready form.
+    /// </summary>
+    /// <remarks>
+    ///   The <see cref="IExtractionStep"/> interface is a companion to the <see cref="IFieldExtractor"/> interface,
+    ///   separating the logic for what to do with a piece of extracted data from how to obtain it. The data produced
+    ///   by executing an <see cref="IExtractionStep"/> instance is fully decomposed and has had an opportunity to
+    ///   undergo intrinsic transformation into a database-ready form.
+    /// </remarks>
+    public interface IExtractionStep {
+        /// <summary>
+        ///   The <see cref="Type"/> of source object on which this <see cref="IExtractionStep"/> is capable of being
+        ///   executed.
+        /// </summary>
+        Type ExpectedSource { get; }
+
+        /// <summary>
+        ///   Execute this <see cref="IExtractionStep"/> on a source object, producing an ordered sequence of values
+        ///   that can be stored in a back-end database.
+        /// </summary>
+        /// <remarks>
+        ///   <para>
+        ///     Every extraction plan has two phases of data transformation. The first is the intrinsic phase, which is
+        ///     generally reserved for type conversions (e.g. converting C# enumerations into strings or integers). The
+        ///     second is the extrinsic phase, which allows users to define custom ad hoc conversions on an
+        ///     entity-by-entity, and even field-by-field, basis. The values produced by executing an
+        ///     <see cref="IExtractionStep"/> will have completed the first transformation phase only.
+        ///   </para>
+        ///   <para>
+        ///     Executing an <see cref="IExtractionStep"/> on a <see langword="null"/> source object will produce a
+        ///     sequence of <see cref="DBValue.NULL"/> of the appropriate length. This ensures that a particular
+        ///     <see cref="IExtractionStep"/> instance always produces sequences of equal length independent of the
+        ///     source object's nullity.
+        ///   </para>
+        /// </remarks>
+        /// <param name="sourceObject">
+        ///   The source object on which to execute this <see cref="IExtractionStep"/>.
+        /// </param>
+        /// <pre>
+        ///   If <paramref name="sourceObject"/> is not <see langword="null"/>, then <see cref="ExpectedSource"/> is
+        ///   its dynamic type or is a base class or interface thereof.
+        /// </pre>
+        /// <returns>
+        ///   An immutable, indexable, ordered sequence of <see cref="DBValue">database values</see> extracted from
+        ///   <paramref name="sourceObject"/>.
+        /// </returns>
+        IReadOnlyList<DBValue> Execute(object? sourceObject);
+    }
+}
---
+++ test/UnitTests/Kvasir/Extraction/FieldExtractors.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Kvasir.Extraction;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UT.Kvasir.Extraction {
+    [TestClass, TestCategory("ReadPropertyExtractor")]
+    public class ReadPropertyExtractorTests {
+        [TestMethod] public void Construct() {
+            // Arrange
+            var prop = typeof(string).GetProperty(nameof(string.Length))!;
+
+            // Act
+            var extractor = new ReadPropertyExtractor(prop);
+
+            // Assert
+            extractor.FieldType.Should().Be(prop.PropertyType);
+            extractor.ExpectedSource.Should().Be(prop.ReflectedType);
+        }
+
+        [TestMethod] public void ExtractFromExact() {
+            // Arrange
+            var prop = typeof(string).GetProperty(nameof(string.Length))!;
+            var extractor = new ReadPropertyExtractor(prop);
+            var source = "Oconomowoc";
+
+            // Act
+            var value = extractor.Execute(source);
+
+            // Assert
+            value.Should().Be(source.Length);
+        }
+
+        [TestMethod] public void ExtractFromDerived() {
+            // Arrange
+            var prop = typeof(Exception).GetProperty(nameof(Exception.HResult))!;
+            var extractor = new ReadPropertyExtractor(prop);
+            var source = new ArgumentException();
+
+            // Act
+            var value = extractor.Execute(source);
+
+            // Assert
+            value.Should().Be(source.HResult);
+        }
+
+        [TestMethod] public void ExtractFromNull() {
+            // Arrange
+            var prop = typeof(string).GetProperty(nameof(string.Length))!;
+            var extractor = new ReadPropertyExtractor(prop);
+            string? source = null;
+
+            // Act
+            var value = extractor.Execute(source);
+
+            // Asert
+            value.Should().BeNull();
+        }
+    }
+}
