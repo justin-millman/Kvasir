bfea986520978295eab6a723e349ea9e0cf83e8a
Justin Millman <jsmillman@msn.com>
2022-06-25 18:28:16 -0500

    IdentityExtractor<T>
    This commit adds IdentityExtractor<T>, a new IFieldExtractor implementation that returns its input directly. This is
    intended to be used when extracting from collections of primitives, in which case there is no Entity on which to perform
    a property read.

---
+++ src/Kvasir/Extraction/IdentityExtractor.cs
@@ -0,0 +1,34 @@
+﻿using Cybele.Extensions;
+using System;
+using System.Diagnostics;
+
+namespace Kvasir.Extraction {
+    /// <summary>
+    ///   An <see cref="IFieldExtractor"/> that simply returns the value it was provided.
+    /// </summary>
+    /// <remarks>
+    ///   The <see cref="IdentityExtractor{T}"/> class is intended to be used with collection elements, where primitive
+    ///   values may be stored directly. In such circumstances, there is no wrapping Entity and therefore no property
+    ///   or function that can be used to access the value. Note, however, that if the element type of a collection is
+    ///   itself a complex type (either an Entity or an Aggregate), the <see cref="IdentityExtractor{T}"/> should not
+    ///   be used.
+    /// </remarks>
+    public sealed class IdentityExtractor<T> : IFieldExtractor {
+        /// <inheritdoc/>
+        public Type ExpectedSource => typeof(T);
+
+        /// <inheritdoc/>
+        public Type FieldType => typeof(T);
+
+        /// <summary>
+        ///   Constructs a new <see cref="IdentityExtractor{T}"/>.
+        /// </summary>
+        internal IdentityExtractor() {}
+
+        /// <inheritdoc/>
+        public object? Execute(object? source) {
+            Debug.Assert(source is null || source.GetType().IsInstanceOf(ExpectedSource));
+            return source;
+        }
+    }
+}
