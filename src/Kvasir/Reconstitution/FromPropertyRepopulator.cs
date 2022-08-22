986a2365c93957296a48c88d57ef5393c5840ace
Justin Millman <jsmillman@msn.com>
2022-08-21 19:36:46 -0500

    Extend Reconstitution Components to Work with PropertyChains
    This commit adds an IFieldExtractor argument to the SetPropertyMutationStep and the FromPropertyRepopulator to enable
    them to function properly if the target property is not actually on the entity source object. The new constructor
    argument controls how the target object is obtained from the initial source, which allows these classes to interact with
    content nested in Aggregate-type Fields. This is necessary to support Relations modeled in this fashion, as well as
    read-only Aggregate-type Fields that expose writeability on their constituent properties.

--- src/Kvasir/Reconstitution/FromPropertyRepopulator.cs
+++ src/Kvasir/Reconstitution/FromPropertyRepopulator.cs
@@ -1,5 +1,6 @@
 ﻿using Ardalis.GuardClauses;
 using Cybele.Extensions;
+using Kvasir.Extraction;
 using Kvasir.Relations;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,10 @@ namespace Kvasir.Reconstitution {
         /// <summary>
         ///   Constructs a new <see cref="FromPropertyRepopulator"/>.
         /// </summary>
+        /// <param name="extractor">
+        ///   The <see cref="IFieldExtractor"/> describing how to access the object on which the
+        ///   <see cref="IRelation"/> resides.
+        /// </param>
         /// <param name="property">
         ///   The <see cref="PropertyInfo"/> describing the property from which to read the <see cref="IRelation"/>
         ///   instance to be populated.
@@ -25,18 +30,24 @@ namespace Kvasir.Reconstitution {
         /// <pre>
         ///   <paramref name="property"/> is a readable instance property
         ///     --and--
+        ///   <paramref name="property"/> is accessible from the <see cref="IFieldExtractor.FieldType">type produced by
+        ///   <paramref name="extractor"/></see>.
+        ///     --and--
         ///   the <see cref="PropertyInfo.PropertyType"/> of <paramref name="property"/> is <see cref="IRelation"/> or
         ///   a type that implements the <see cref="IRelation"/> interface.
         /// </pre>
-        internal FromPropertyRepopulator(PropertyInfo property) {
+        internal FromPropertyRepopulator(IFieldExtractor extractor, PropertyInfo property) {
+            Guard.Against.Null(extractor, nameof(extractor));
             Guard.Against.Null(property, nameof(property));
             Debug.Assert(property.PropertyType.IsInstanceOf(typeof(IRelation)));
             Debug.Assert(property.CanRead);
             Debug.Assert(!property.GetGetMethod()!.IsStatic);
             Debug.Assert(property.ReflectedType is not null);
+            Debug.Assert(extractor.FieldType.IsInstanceOf(property.ReflectedType!));
 
+            extractor_ = extractor;
             property_ = property;
-            ExpectedSubject = property.ReflectedType!;
+            ExpectedSubject = extractor.ExpectedSource;
         }
 
         /// <inheritdoc/>
@@ -44,13 +55,15 @@ namespace Kvasir.Reconstitution {
             Debug.Assert(subject.GetType().IsInstanceOf(ExpectedSubject));
             Debug.Assert(!entries.IsEmpty());
 
-            var relation = (IRelation)property_.GetValue(subject)!;
+            var target = extractor_.Execute(subject);
+            var relation = (IRelation)property_.GetValue(target)!;
             foreach (var entry in entries) {
                 relation.Repopulate(entry);
             }
         }
 
 
+        private readonly IFieldExtractor extractor_;
         private readonly PropertyInfo property_;
     }
 }
