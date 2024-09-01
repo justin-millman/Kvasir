3efb1886d3f7aa2dee41d9e4467acf3c5d3df33a
Justin Millman <jsmillman@msn.com>
2024-09-01 14:59:27 -0500

    Entity Depot
    This commit implements the EntityDepot, a thing wrapper around a map for storing the active Entities in an application
    at a given time. This utility is necessary for reconstiting References, where we need a standalone collection of the
    Entities for a given type to perform lookup on.

---
+++ src/Kvasir/Core/EntityDepot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Kvasir.Core {
+    /// <summary>
+    ///   A collection of Entities, indexable by <see cref="Type"/>.
+    /// </summary>
+    internal sealed class EntityDepot {
+        /// <summary>
+        ///   Get all the (untyped) Entities of a particular <see cref="Type"/> that have been stored.
+        /// </summary>
+        /// <param name="type">
+        ///   [GET] The <see cref="Type"/> of Entity.
+        /// </param>
+        /// <seealso cref="GetEntities{TEntity}"/>
+        public IEnumerable<object> this[Type type] {
+            get {
+                Debug.Assert(type is not null);
+                entities_.TryAdd(type, new HashSet<object>(ReferenceEqualityComparer.Instance));
+                return entities_[type];
+            }
+        }
+
+        /// <summary>
+        ///   Constructs a new <see cref="EntityDepot"/> with no stored Entities.
+        /// </summary>
+        public EntityDepot() {
+            entities_ = new Dictionary<Type, HashSet<object>>();
+        }
+
+        /// <summary>
+        ///   Get all the (typed) Entities of a particular <see cref="Type"/> that have been stored.
+        /// </summary>
+        /// <seealso cref="this[Type]"/>
+        public IEnumerable<TEntity> GetEntities<TEntity>() where TEntity : class {
+            return this[typeof(TEntity)].Cast<TEntity>();
+        }
+
+        /// <summary>
+        ///   Store a new Entity.
+        /// </summary>
+        /// <param name="entity">
+        ///   The Entity to store.
+        /// </param>
+        public void StoreEntity(object entity) {
+            Debug.Assert(entity is not null);
+            Debug.Assert(entity.GetType().IsClass);
+
+            (this[entity.GetType()] as HashSet<object>)!.Add(entity);
+        }
+
+
+        private readonly Dictionary<Type, HashSet<object>> entities_;
+    }
+}
---
+++ test/UnitTests/Kvasir/Core/EntityDepot.cs
@@ -0,0 +1,99 @@
+﻿using FluentAssertions;
+using Kvasir.Core;
+using Kvasir.Transcription;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UT.Kvasir.Core {
+    [TestClass, TestCategory("EntityDepot")]
+    public class EntityDepotTests {
+        [TestMethod] public void AddEntityFirstOfType() {
+            // Arrange
+            var depot = new EntityDepot();
+            var entity = "Garrison Oaks";
+
+            // Act
+            depot.StoreEntity(entity);
+            var untyped = depot[typeof(string)];
+            var typed = depot.GetEntities<string>();
+
+            // Assert
+            untyped.Should().HaveCount(1);
+            typed.Should().HaveCount(1);
+            untyped.Should().Contain(entity);
+            typed.Should().Contain(entity);
+        }
+
+        [TestMethod] public void AddEntityRepeatOfType() {
+            // Arrange
+            var depot = new EntityDepot();
+            var entity1 = "T'Telir";
+            var entity2 = "Basin City";
+
+            // Act
+            depot.StoreEntity(entity1);
+            depot.StoreEntity(entity2);
+            var untyped = depot[typeof(string)];
+            var typed = depot.GetEntities<string>();
+
+            // Assert
+            untyped.Should().HaveCount(2);
+            typed.Should().HaveCount(2);
+            untyped.Should().Contain(entity1);
+            untyped.Should().Contain(entity2);
+            typed.Should().Contain(entity1);
+            typed.Should().Contain(entity2);
+        }
+
+        [TestMethod] public void AddSameEntityTwice() {
+            // Arrange
+            var depot = new EntityDepot();
+            var entity = "Rarohenga";
+
+            // Act
+            depot.StoreEntity(entity);
+            depot.StoreEntity(entity);
+            var untyped = depot[typeof(string)];
+            var typed = depot.GetEntities<string>();
+
+            // Assert
+            untyped.Should().HaveCount(1);
+            typed.Should().HaveCount(1);
+            untyped.Should().Contain(entity);
+            typed.Should().Contain(entity);
+        }
+
+        [TestMethod] public void AddEqualEntities() {
+            // Arrange
+            var depot = new EntityDepot();
+            var entity1 = new SqlSnippet("Genoa City");
+            var entity2 = new SqlSnippet("Genoa City");
+
+            // Act
+            depot.StoreEntity(entity1);
+            depot.StoreEntity(entity2);
+            var untyped = depot[typeof(SqlSnippet)];
+            var typed = depot.GetEntities<SqlSnippet>();
+
+            // Assert
+            untyped.Should().HaveCount(2);
+            typed.Should().HaveCount(2);
+            untyped.Should().Contain(entity1);
+            untyped.Should().Contain(entity2);
+            typed.Should().Contain(entity1);
+            typed.Should().Contain(entity2);
+        }
+
+        [TestMethod] public void LookupEntityTypeNoResults() {
+            // Arrange
+            var depot = new EntityDepot();
+
+            // Act
+            var untyped = depot[typeof(string)];
+            var typed = depot.GetEntities<string>();
+
+            // Assert
+            untyped.Should().BeEmpty();
+            typed.Should().BeEmpty();
+        }
+    }
+}
