1ebbdb8afe4fbbb9e6fe425d22196ddc34a37018
Justin Millman <jsmillman@msn.com>
2024-09-02 13:03:24 -0500

    Ensure that DefaultStructCreator can Produce Null from Row of All Nulls
    This commit extends the behavior of the DefaultStructCreator to match that of the ConstructingCreator when dealing with
    a row of all null values. Specifically, the DefaultStructCreator now takes an argument controlling whether or not a type
    can be created from a row of all null values. Even though the arguments won't be passed to the default constructor
    (obviously), we still have to handle this case to avoid creating an object in circumstances when we want to have a null
    value.

--- src/Kvasir/Reconstitution/DefaultStructCreator.cs
+++ src/Kvasir/Reconstitution/DefaultStructCreator.cs
@@ -3,6 +3,7 @@ using Kvasir.Schema;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Kvasir.Reconstitution {
     /// <summary>
@@ -23,19 +24,39 @@ namespace Kvasir.Reconstitution {
         /// <param name="type">
         ///   The <see cref="ResultType"/> of the new <see cref="DefaultStructCreator"/>.
         /// </param>
-        public DefaultStructCreator(Type type) {
+        /// <param name="allowAllNulls">
+        ///   If <see langword="true"/>, then a new non-<see langword="null"/> CLR object will be constructed even if
+        ///   each of the <see cref="DBValue">database values</see> provided is <see cref="DBValue.NULL"/>. If
+        ///   <see langword="false"/>, then such a set of values will result in a <see langword="null"/> object.
+        /// </param>
+        public DefaultStructCreator(Type type, bool allowAllNulls) {
             Debug.Assert(type is not null);
             Debug.Assert(!type.IsPrimitive && !type.IsEnum && type.IsValueType);
+
             type_ = type;
+            constructFromAllNulls_ = allowAllNulls;
         }
 
         /// <inheritdoc/>
         public object? CreateFrom(IReadOnlyList<DBValue> dbValues) {
             Debug.Assert(dbValues is not null && !dbValues.IsEmpty());
-            return Activator.CreateInstance(type_);
+
+            // If all the values in the row are null, it means one of two things: either we're dealing with a nullable
+            // Aggregate (constructFromNulls_ will be true) or we're dealing with a non-nullable Aggregate (in which
+            // case constructFromNulls_ will be false). In the former case, the Translation Layer ensures that at least
+            // one of the nested Fields is itself non-nullable, and it may be possible that running the argument
+            // creators induces an error.
+
+            if (!constructFromAllNulls_ && dbValues.All(v => v == DBValue.NULL)) {
+                return null;
+            }
+            else {
+                return Activator.CreateInstance(type_);
+            }
         }
 
 
         private readonly Type type_;
+        private readonly bool constructFromAllNulls_;
     }
 }
