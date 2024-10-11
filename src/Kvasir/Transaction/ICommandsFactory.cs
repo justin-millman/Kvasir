2b3cfd8a54c8975f3de950aaaaae179dac677256
Justin Millman <jsmillman@msn.com>
2024-10-11 11:26:42 -0500

    Transaction Commands Interfaces
    This commit adds two interfaces: ICommands and ICommandsFactory. The former is responsible for producing DBCommands for
    CRUD operations against a particular Table, and the latter is responsible for creating the former. Conceptually, an
    implementation of the ICommandsFactory interface will correspond to a particular back-end database system and/or SQL
    flavor. These are the APIs that the Kvasir framework will interact with to load and store data, enabling support for
    arbitrary RDBMSes.

---
+++ src/Kvasir/Transaction/ICommandsFactory.cs
@@ -0,0 +1,24 @@
+﻿using Kvasir.Schema;
+
+namespace Kvasir.Transaction {
+    /// <summary>
+    ///   The interface describing how to create <see cref="ICommands"/> instances.
+    /// </summary>
+    /// <remarks>
+    ///   The purpose of this interface is to provide a mechanism for the framework to create instances of the
+    ///   <see cref="ICommands"/> interface for constituent tables without knowing anything about the particular
+    ///   implementation details. An implementation of the <see cref="ICommandsFactory"/> interface should generally
+    ///   correspond to a particular back-end database provider (e.g. MySQL) and produce instances that can operate
+    ///   against that RDBMS.
+    /// </remarks>
+    public interface ICommandsFactory {
+        /// <summary>
+        ///   Get the <see cref="ICommands"/> that defines database interactions against a particular
+        ///   <see cref="ITable">Table</see>.
+        /// </summary>
+        /// <param name="table">
+        ///   The <see cref="ITable">Table</see>.
+        /// </param>
+        ICommands CreateCommand(ITable table);
+    }
+}
