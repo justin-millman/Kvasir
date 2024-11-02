2c930e8160ef43ebff9fefcd1dc2fe889581af30
Justin Millman <jsmillman@msn.com>
2024-11-02 09:40:03 -0500

    Transactor: CreateTables
    This commit defines the initial shape of the Transactor class and implements the table creation API. The Transactor is
    the component that is responsible for bridging the gap between the C#/CLR world and the database world. It uses various
    interfaces to generate the appropriate SQL commands for a given Table (that is, the Transactor knows nothing about the
    actual logic required to do something) and ensures that execution is properly ordered, formally transacted, etc.
    
    For creating tables, the logic is fairly straightforward. The most complicated piece is the topological sort required to
    ensure that dependent Tables are created before depending Tables. Principal Tables are always created first, followed by
    all Relation Tables. Tables are created as a single atomic transaction, and we are able to handle both failed commits
    (via rollback) and failed rollbacks.
    
    For testing, a custom fixture has been created that sets up all the necessary mocks, since this is a very mock-heavy
    scenario. The use of interfaces throughout the Transactor allows for seamless use of arbitrary back-end RDBMS systems
    (including custom implementations like JSON dumping) without altering the core business logic. A nice side effect of
    this design is easy mockability via dependency injection.

---
+++ test/UnitTests/Kvasir/Transaction/TableCreation.cs
@@ -0,0 +1,185 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+
+using static UT.Kvasir.Transaction.TableCreation;
+
+namespace UT.Kvasir.Transaction {
+    [TestClass, TestCategory("Table Creation")]
+    public class TableCreationTests {
+        [TestMethod] public void SingleEntityNoRelations() {
+            // Arrange
+            var fixture = new TestFixture(typeof(PoliticalRally));
+
+            // Act
+            fixture.Transactor.CreateTables();
+            var rallyCmd = fixture.PrincipalCommands<PoliticalRally>().CreateTableCommand;
+
+            // Assert
+            rallyCmd.Connection.Should().Be(fixture.Connection);
+            rallyCmd.Transaction.Should().Be(fixture.Transaction);
+            fixture.ShouldBeOrdered(rallyCmd);
+            fixture.Transaction.Received(1).Commit();
+        }
+
+        [TestMethod] public void SingleEntityScalarRelations() {
+            // Arrange
+            var fixture = new TestFixture(typeof(Yogurt));
+
+            // Act
+            fixture.Transactor.CreateTables();
+            var yogurtCmd = fixture.PrincipalCommands<Yogurt>().CreateTableCommand;
+            var nutritionCmd = fixture.RelationCommands<Yogurt>(0).CreateTableCommand;
+            var traitsCmd = fixture.RelationCommands<Yogurt>(1).CreateTableCommand;
+
+            // Assert
+            yogurtCmd.Connection.Should().Be(fixture.Connection);
+            yogurtCmd.Transaction.Should().Be(fixture.Transaction);
+            nutritionCmd.Connection.Should().Be(fixture.Connection);
+            nutritionCmd.Transaction.Should().Be(fixture.Transaction);
+            traitsCmd.Connection.Should().Be(fixture.Connection);
+            traitsCmd.Transaction.Should().Be(fixture.Transaction);
+            fixture.ShouldBeOrdered(yogurtCmd, (nutritionCmd, traitsCmd));
+            fixture.Transaction.Received(1).Commit();
+        }
+
+        [TestMethod] public void MultipleUnrelatedEntities() {
+            // Arrange
+            var fixture = new TestFixture(typeof(Gondola), typeof(Scooter), typeof(Muffin));
+
+            // Act
+            fixture.Transactor.CreateTables();
+            var gondolaCmd = fixture.PrincipalCommands<Gondola>().CreateTableCommand;
+            var scooterCmd = fixture.PrincipalCommands<Scooter>().CreateTableCommand;
+            var muffinCmd = fixture.PrincipalCommands<Muffin>().CreateTableCommand;
+
+            // Assert
+            gondolaCmd.Connection.Should().Be(fixture.Connection);
+            gondolaCmd.Transaction.Should().Be(fixture.Transaction);
+            scooterCmd.Connection.Should().Be(fixture.Connection);
+            scooterCmd.Transaction.Should().Be(fixture.Transaction);
+            muffinCmd.Connection.Should().Be(fixture.Connection);
+            muffinCmd.Transaction.Should().Be(fixture.Transaction);
+            fixture.ShouldBeOrdered((gondolaCmd, scooterCmd, muffinCmd));
+            fixture.Transaction.Received(1).Commit();
+        }
+
+        [TestMethod] public void MultipleEntitiesRelatedByReferenceChain() {
+            // Arrange
+            var fixture = new TestFixture(typeof(EpicRapBattle), typeof(EpicRapBattle.Rapper), typeof(EpicRapBattle.Actor));
+
+            // Act
+            fixture.Transactor.CreateTables();
+            var rapBattleCmd = fixture.PrincipalCommands<EpicRapBattle>().CreateTableCommand;
+            var rapperCmd = fixture.PrincipalCommands<EpicRapBattle.Rapper>().CreateTableCommand;
+            var actorCmd = fixture.PrincipalCommands<EpicRapBattle.Actor>().CreateTableCommand;
+
+            // Assert
+            rapBattleCmd.Connection.Should().Be(fixture.Connection);
+            rapBattleCmd.Transaction.Should().Be(fixture.Transaction);
+            rapperCmd.Connection.Should().Be(fixture.Connection);
+            rapperCmd.Transaction.Should().Be(fixture.Transaction);
+            actorCmd.Connection.Should().Be(fixture.Connection);
+            actorCmd.Transaction.Should().Be(fixture.Transaction);
+            fixture.ShouldBeOrdered(actorCmd, rapperCmd, rapBattleCmd);
+            fixture.Transaction.Received(1).Commit();
+        }
+
+        [TestMethod] public void MultipleEntitiesRelatedByReferenceTree() {
+            // Arrange
+            var fixture = new TestFixture(typeof(JackOLantern), typeof(Farmer.Farm), typeof(Farmer));
+
+            // Act
+            fixture.Transactor.CreateTables();
+            var lanternCmd = fixture.PrincipalCommands<JackOLantern>().CreateTableCommand;
+            var farmerCmd = fixture.PrincipalCommands<Farmer>().CreateTableCommand;
+            var farmCmd = fixture.PrincipalCommands<Farmer.Farm>().CreateTableCommand;
+
+            // Assert
+            lanternCmd.Connection.Should().Be(fixture.Connection);
+            lanternCmd.Transaction.Should().Be(fixture.Transaction);
+            farmerCmd.Connection.Should().Be(fixture.Connection);
+            farmCmd.Transaction.Should().Be(fixture.Transaction);
+            farmCmd.Connection.Should().Be(fixture.Connection);
+            farmCmd.Transaction.Should().Be(fixture.Transaction);
+            fixture.ShouldBeOrdered(farmCmd, (lanternCmd, farmerCmd));
+            fixture.Transaction.Received(1).Commit();
+        }
+
+        [TestMethod] public void MultipleEntitiesRelatedByRelation() {
+            // Arrange
+            var fixture = new TestFixture(typeof(CashRegister), typeof(CashRegister.Item), typeof(CashRegister.Currency));
+
+            // Act
+            fixture.Transactor.CreateTables();
+            var registerCmd = fixture.PrincipalCommands<CashRegister>().CreateTableCommand;
+            var itemCmd = fixture.PrincipalCommands<CashRegister.Item>().CreateTableCommand;
+            var currencyCmd = fixture.PrincipalCommands<CashRegister.Currency>().CreateTableCommand;
+            var cashCmd = fixture.RelationCommands<CashRegister>(0).CreateTableCommand;
+            var sellablesCmd = fixture.RelationCommands<CashRegister>(1).CreateTableCommand;
+
+            // Assert
+            registerCmd.Connection.Should().Be(fixture.Connection);
+            registerCmd.Transaction.Should().Be(fixture.Transaction);
+            itemCmd.Connection.Should().Be(fixture.Connection);
+            itemCmd.Transaction.Should().Be(fixture.Transaction);
+            currencyCmd.Connection.Should().Be(fixture.Connection);
+            currencyCmd.Transaction.Should().Be(fixture.Transaction);
+            cashCmd.Connection.Should().Be(fixture.Connection);
+            cashCmd.Transaction.Should().Be(fixture.Transaction);
+            sellablesCmd.Connection.Should().Be(fixture.Connection);
+            sellablesCmd.Transaction.Should().Be(fixture.Transaction);
+            fixture.ShouldBeOrdered((registerCmd, itemCmd), sellablesCmd);
+            fixture.ShouldBeOrdered((registerCmd, currencyCmd), cashCmd);
+            fixture.Transaction.Received(1).Commit();
+        }
+
+        [TestMethod] public void SelfReferentialEntity() {
+            // Arrange
+            var fixture = new TestFixture(typeof(Matrix));
+
+            // Act
+            fixture.Transactor.CreateTables();
+            var matrixCmd = fixture.PrincipalCommands<Matrix>().CreateTableCommand;
+            var eigenvaluesCmd = fixture.RelationCommands<Matrix>(0).CreateTableCommand;
+            var inversesCmd = fixture.RelationCommands<Matrix>(1).CreateTableCommand;
+
+            // Assert
+            matrixCmd.Connection.Should().Be(fixture.Connection);
+            matrixCmd.Transaction.Should().Be(fixture.Transaction);
+            eigenvaluesCmd.Connection.Should().Be(fixture.Connection);
+            eigenvaluesCmd.Transaction.Should().Be(fixture.Transaction);
+            inversesCmd.Connection.Should().Be(fixture.Connection);
+            inversesCmd.Transaction.Should().Be(fixture.Transaction);
+            fixture.ShouldBeOrdered(matrixCmd, (eigenvaluesCmd, inversesCmd));
+            fixture.Transaction.Received(1).Commit();
+        }
+
+        [TestMethod] public void TransactionRolledBack() {
+            // Arrange
+            var fixture = new TestFixture(typeof(Bond)).WithCommitError();
+
+            // Act
+            var action = () => fixture.Transactor.CreateTables();
+
+            // Assert
+            action.Should().ThrowExactly<InvalidOperationException>();
+            fixture.Transaction.Received(1).Commit();
+            fixture.Transaction.Received(1).Rollback();
+        }
+
+        [TestMethod] public void RollbackFails() {
+            // Arrange
+            var fixture = new TestFixture(typeof(GrilledCheese)).WithRollbackError();
+
+            // Act
+            var action = () => fixture.Transactor.CreateTables();
+
+            // Assert
+            action.Should().ThrowExactly<AggregateException>();
+            fixture.Transaction.Received(1).Commit();
+            fixture.Transaction.Received(1).Rollback();
+        }
+    }
+}
---
+++ test/UnitTests/Kvasir/Transaction/_Entities.cs
@@ -0,0 +1,172 @@
+﻿using Kvasir.Annotations;
+using Kvasir.Relations;
+using System;
+
+namespace UT.Kvasir.Transaction {
+    internal static class TableCreation {
+        // Test Scenario: Single Entity without Relations
+        public class PoliticalRally {
+            [PrimaryKey] public Guid ID { get; set; }
+            public string Candidate { get; set; } = "";
+            public DateTime Date { get; set; }
+            public ushort Attendance { get; set; }
+            public bool IsNationalConvention { get; set; }
+            public string KeynoteSpeaker { get; set; } = "";
+        }
+
+        // Test Scenario: Single Entity with Scalar Relations
+        public class Yogurt {
+            public enum Fruit { Cherry, Banana, Orange, Strawberry, Raspberry, Blueberry, Lemon, Passionfruit }
+            public enum Trait { Cultured, NonFat, LowFat, Organic, DairyFree }
+
+            [PrimaryKey] public Guid ProductID { get; set; }
+            public string Brand { get; set; } = "";
+            public Fruit Flavor { get; set; }
+            public RelationMap<string, double> NutritionInfo { get; } = new();
+            public RelationSet<Trait> Traits { get; } = new();
+        }
+
+        // Test Scenario: Multiple Unrelated Entities
+        public class Gondola {
+            [PrimaryKey] public Guid BoatID { get; set; }
+            public double Length { get; set; }
+            public bool Venetian { get; set; }
+            public byte MaxCapacity { get; set; }
+            public string? MainGondolier { get; set; }
+        }
+        public class Scooter {
+            public enum Kind { Electric, Mobility, Segway, Water }
+
+            [PrimaryKey] public Guid ProductID { get; set; }
+            public decimal Price { get; set; }
+            public float TopSpeed { get; set; }
+            public Kind Variety { get; set; }
+            public bool IsSingleRider { get; set; }
+            public int CasualtiesCaused { get; set; }
+        }
+        public class Muffin {
+            [PrimaryKey] public Guid MuffinID { get; set; }
+            public string Flavor { get; set; } = "";
+            public string SoldBy { get; set; } = "";
+            public decimal Price { get; set; }
+            public uint Calories { get; set; }
+            public bool InBasket { get; set; }
+            public bool FromBran { get; set; }
+        }
+
+        // Test Scenario: Multiple Entities Related via Reference Chain
+        public class EpicRapBattle {
+            public class Actor {
+                [PrimaryKey] public ulong IMDBNumber { get; set; }
+                public string FirstName { get; set; } = "";
+                public string LastName { get; set; } = "";
+                public DateTime DateOfBirth { get; set; }
+            }
+            public class Rapper {
+                [PrimaryKey] public Guid PersonID { get; set; }
+                public string Name { get; set; } = "";
+                public bool IsRealPerson { get; set; }
+                public Actor Portrayer { get; set; } = new();
+                public string Lines { get; set; } = "";
+            }
+
+            [PrimaryKey] public uint EpisodeNumber { get; set; }
+            public string YouTubeURL { get; set; } = "";
+            public Rapper Contestant1 { get; set; } = new();
+            public Rapper Contestant2 { get; set; } = new();
+            public DateTime OriginalAirdate { get; set; }
+        }
+
+        // Test Scenario: Multiple Entities Related via Reference Tree
+        public class JackOLantern {
+            public enum Gourd { Pumpkin, Squash }
+
+            [PrimaryKey] public Guid ID { get; set; }
+            public Gourd MadeFrom { get; set; }
+            public double Weight { get; set; }
+            public ulong NumSeeds { get; set; }
+            public bool Candlelit { get; set; }
+            public Farmer.Farm PumpkinPatch { get; set; } = new();
+        }
+        public class Farmer {
+            public class Farm {
+                [PrimaryKey] public Guid FarmID { get; set; }
+                public float Acreage { get; set; }
+                public decimal AnnualGross { get; set; }
+                public string? RegisteredName { get; set; }
+                public bool FederallyInsured { get; set; }
+            }
+
+            [PrimaryKey] public string SSN { get; set; } = "";
+            public string Name { get; set; } = "";
+            public Farm PrincipalFarm { get; set; } = new();
+            public Farm? BackUpFarm { get; set; } = new();
+            public decimal NetWorth { get; set; }
+            public ushort NumTractorsOwned { get; set; }
+        }
+
+        // Test Scenario: Multiple Entities Related via Relation
+        public class CashRegister {
+            public class Item {
+                [PrimaryKey] public Guid ID { get; set; }
+                public string Name { get; set; } = "";
+                public decimal DollarCost { get; set; }
+                public uint Inventory { get; set; }
+            }
+            public class Currency {
+                [PrimaryKey] public string Name { get; set; } = "";
+                public char Symbol { get; set; }
+                public bool IsVirtual { get; set; }
+                public decimal DollarExchangeRate { get; set; }
+            }
+
+            [PrimaryKey] public Guid RegisterID { get; set; }
+            public string Location { get; set; } = "";
+            public RelationMap<Currency, ushort> CashOnHand { get; set; } = new RelationMap<Currency, ushort>();
+            public RelationSet<Item> Sellables { get; set; } = new RelationSet<Item>();
+            public double Weight { get; set; }
+            public bool IsDigital { get; set; }
+            public string AdminCode { get; set; } = "";
+        }
+
+        // Test Scenario: Single Entity with Self-Referential Relation
+        public class Matrix {
+            [Flags] public enum Traits { None = 0, Identity = 1, Diagonal = 2, Triangular = 4, Square = 8, Definite = 16, Unitary = 32 }
+
+            [PrimaryKey] public Guid MatrixID { get; set; }
+            public ushort NumRows { get; set; }
+            public ushort NumColumns { get; set; }
+            public IReadOnlyRelationList<double> Eigenvalues { get; set; } = new RelationList<double>();
+            public RelationSet<Matrix> Inverses { get; set; } = new RelationSet<Matrix>();
+            public Traits MatrixTraits { get; set; }
+        }
+
+        // Test Scenario: Transaction Rolled Back
+        public class Bond {
+            public enum Category { Corporate, Municipal, Treasury, Foreign }
+
+            [PrimaryKey] public Guid BondNumber { get; set; }
+            public Category Kind { get; set; }
+            public string Issuer { get; set; } = "";
+            public decimal Par { get; set; }
+            public double CouponRate { get; set; }
+            public DateTime Maturity { get; set; }
+            public decimal? CallValue { get; set; }
+        }
+
+        // Test Scenario: Rollback Fails
+        public class GrilledCheese {
+            [Flags] public enum Cheese { American = 1, Swiss = 2, Gouda = 4, Goat = 8, PepperJack = 16, Gruyere = 32, Muenster = 64, Other = 128 }
+            public enum Bread { White, Rye, Pumpernickel, Sourdough, TexasToast, WholeGrain, Other }
+
+            [PrimaryKey] public Cheese Cheeses { get; set; }
+            [PrimaryKey] public Bread TopBread { get; set; }
+            [PrimaryKey] public Bread BottomBread { get; set; }
+            [PrimaryKey] public double CheeseToBreadRatio { get; set; }
+            public uint Calories { get; set; }
+            public bool WithCrusts { get; set; }
+            public byte NumPiecesCut { get; set; }
+            public double CheeseFullRating { get; set; }
+        }
+    }
+}
