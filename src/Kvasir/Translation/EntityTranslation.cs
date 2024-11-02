33113551322366017775f8ab7f713013de792f54
Justin Millman <jsmillman@msn.com>
2024-11-02 09:50:12 -0500

    Properly Memoize Translations
    This commit adds memoization for overall Translations. We already have memoization for Principal Tables, which we have
    to keep because those can be translated without doing a full Translation. However, there is no memoization for Relation
    Tables, and therefore attempting to do a full translation of the same source type twice can lead to duplication errors.
    
    I also renamed the Translation struct to EntityTranslation, since otherwise there's weird conflicts with the
    Kvasir.Translation namespace.

--- src/Kvasir/Translation/EntityTranslation.cs
+++ src/Kvasir/Translation/EntityTranslation.cs
@@ -6,10 +6,10 @@ namespace Kvasir.Translation {
     ///   A translation of a single CLR type.
     /// </summary>
     /// 
-    /// <param name="CLRSource">The CLR <see cref="Type"/> that produced this <see cref="Translation"/></param>
+    /// <param name="CLRSource">The CLR <see cref="Type"/> that produced this <see cref="EntityTranslation"/></param>
     /// <param name="Principal">The definition of the Principal Table</param>
     /// <param name="Relations">The definitions of any Relation Tables</param>
-    internal sealed record class Translation(
+    internal sealed record class EntityTranslation(
         Type CLRSource,
         PrincipalTableDef Principal,
         IReadOnlyList<RelationTableDef> Relations
