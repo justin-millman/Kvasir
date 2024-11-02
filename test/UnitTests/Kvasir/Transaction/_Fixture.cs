ee977ff8e8db28d7ec7bf7d168a2eb072cb3ff90
Justin Millman <jsmillman@msn.com>
2024-11-02 09:40:03 -0500

    Transactor: CreateTables
    This commit defines the initial shape of the Transactor class and implements the table creation API. The Transactor is
    the component that is responsible for bridging the gap between the C#/CLR world and the database world. It uses various
    interfaces to generate the appropriate SQL commands for a given Table (that is, the Transactor knows nothing about the
    actual logic required to do something) and ensures that execution is properly ordered, formally transacted, etc.
    
    For creating tables, the logic is fairly straightforward. The most complicated piece is the topological sort required to
    ensure that dependent Tables are created before depending Tables. Principal Tables are always created first, followed by
    all Relation Tables. Tables are created as a single atomic transaction, and we are able to handle both failed commits
    (via rollback) and failed rollbacks.
    
    For testing, a custom fixture has been created that sets up all the necessary mocks, since this is a very mock-heavy
    scenario. The use of interfaces throughout the Transactor allows for seamless use of arbitrary back-end RDBMS systems
    (including custom implementations like JSON dumping) without altering the core business logic. A nice side effect of
    this design is easy mockability via dependency injection.

---
+++ test/UnitTests/Kvasir/Transaction/_Fixture.cs
@@ -0,0 +1,111 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using Kvasir.Core;
+using Kvasir.Schema;
+using Kvasir.Transaction;
+using Kvasir.Translation;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace UT.Kvasir.Transaction {
+    internal sealed class TestFixture {
+        public IReadOnlyDictionary<ITable, ICommands> Commands => commands_;
+        public IDbConnection Connection { get; }
+        public IDbTransaction Transaction { get; }
+        public EntityDepot Depot { get; }
+        public Transactor Transactor { get; }
+
+
+        public TestFixture(params Type[] types) {
+            commands_ = new Dictionary<ITable, ICommands>();
+            commandsFactory_ = Substitute.For<ICommandsFactory>();
+            Connection = Substitute.For<IDbConnection>();
+            Transaction = Connection.BeginTransaction();
+            Depot = new EntityDepot();
+            translator_ = new Translator(t => Depot[t]);
+            ordering_ = new Dictionary<IDbCommand, int>();
+
+            Connection.State.Returns(ConnectionState.Open);
+
+            var translations = types.Select(t => translator_[t]);
+            var tables = translations.SelectMany(x => x.Relations.Select(r => r.Table).Append(x.Principal.Table));
+            foreach (var table in tables) {
+                var commands = Substitute.For<ICommands>();
+
+                var createTable = Substitute.For<IDbCommand>();
+                createTable.CommandText = $"CREATE TABLE {table.Name}";
+                commands.CreateTableCommand.Returns(createTable);
+                commands.CreateTableCommand.When(c => c.ExecuteNonQuery()).Do(_ => ordering_[createTable] = ordering_.Count + 1);
+
+                commandsFactory_.CreateCommands(Arg.Is<ITable>(t => t == table)).Returns(commands);
+                commands_[table] = commands;
+            }
+
+            Transactor = new Transactor(translations, Connection, commandsFactory_, e => Depot.StoreEntity(e));
+        }
+        public TestFixture WithCommitError() {
+            Transaction.When(x => x.Commit()).Throw<InvalidOperationException>();
+            return this;
+        }
+        public TestFixture WithRollbackError() {
+            WithCommitError();
+            Transaction.When(x => x.Rollback()).Throw<InvalidOperationException>();
+            return this;
+        }
+
+        public ICommands PrincipalCommands<TEntity>() {
+            var table = translator_[typeof(TEntity)].Principal.Table;
+            return commands_[table];
+        }
+        public ICommands RelationCommands<TEntity>(int index) {
+            var table = translator_[typeof(TEntity)].Relations[index].Table;
+            return commands_[table];
+        }
+
+        [CustomAssertion]
+        public void ShouldBeOrdered(params object[] commands) {
+            int getOrderingOf(IDbCommand cmd) {
+                if (!ordering_.TryGetValue(cmd, out int order)) {
+                    Execute.Assertion
+                        .ForCondition(false)
+                        .FailWith($"Command '{cmd.CommandText}' was never executed");
+                }
+                return order;
+            }
+
+            var previousOrdering = -1;
+            foreach (var command in commands) {
+                if (command is IDbCommand cmd) {
+                    int order = getOrderingOf(cmd);
+                    if (order < previousOrdering) {
+                        Execute.Assertion
+                            .ForCondition(false)
+                            .FailWith($"Command '{cmd.CommandText}' was executed out-of-order");
+                    }
+                    previousOrdering = order;
+                }
+                else {
+                    var group = command as ITuple;
+                    var items = Enumerable.Range(0, group!.Length).Select(i => group[i]).Cast<IDbCommand>();
+                    var sorted = items.Select(Command => (Command, Order: getOrderingOf(Command))).OrderBy(p => p.Order);
+                    if (sorted.First().Order < previousOrdering) {
+                        Execute.Assertion
+                            .ForCondition(false)
+                            .FailWith($"Command '{sorted.First().Command}' was executed out-of-order");
+                    }
+                    previousOrdering = sorted.Last().Order;
+                }
+            }
+        }
+
+
+        private readonly ICommandsFactory commandsFactory_;
+        private readonly Translator translator_;
+        private readonly Dictionary<ITable, ICommands> commands_;
+        private readonly Dictionary<IDbCommand, int> ordering_;
+    }
+}
