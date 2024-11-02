9fed01744e1bd164f756617c8d78eb741deb6cc3
Justin Millman <jsmillman@msn.com>
2024-11-02 09:40:03 -0500

    Transactor: CreateTables
    This commit defines the initial shape of the Transactor class and implements the table creation API. The Transactor is
    the component that is responsible for bridging the gap between the C#/CLR world and the database world. It uses various
    interfaces to generate the appropriate SQL commands for a given Table (that is, the Transactor knows nothing about the
    actual logic required to do something) and ensures that execution is properly ordered, formally transacted, etc.
    
    For creating tables, the logic is fairly straightforward. The most complicated piece is the topological sort required to
    ensure that dependent Tables are created before depending Tables. Principal Tables are always created first, followed by
    all Relation Tables. Tables are created as a single atomic transaction, and we are able to handle both failed commits
    (via rollback) and failed rollbacks.
    
    For testing, a custom fixture has been created that sets up all the necessary mocks, since this is a very mock-heavy
    scenario. The use of interfaces throughout the Transactor allows for seamless use of arbitrary back-end RDBMS systems
    (including custom implementations like JSON dumping) without altering the core business logic. A nice side effect of
    this design is easy mockability via dependency injection.

---
+++ src/Kvasir/Transaction/Topology.cs
@@ -0,0 +1,90 @@
+﻿using Cybele.Extensions;
+using Kvasir.Translation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kvasir.Transaction {
+    /// <summary>
+    ///   Some utility functions for performing topological queries needed by the Transaction Layer.
+    /// </summary>
+    internal static class Topology {
+        /// <summary>
+        ///   Topologically sorts the Principal Table definitions from a set of
+        ///   <see cref="EntityTranslation">translations</see>.
+        /// </summary>
+        /// <remarks>
+        ///   The topological sort represents the order in which the Principal Tables must be created to avoid defining
+        ///   Foreign Key references to tables that do not yet exist. Consequently, it is the same order in which data
+        ///   must be loaded out of the back-end database in order to ensure that references can be resolved to existing
+        ///   CLR objects. If a Principal Table <c>A</c> occurs in the topological order before <c>B</c>, that means
+        ///   there is no chain of reference fields from <c>A</c> to <c>B</c>. Relations are not included in the order
+        ///   because they are all inherently independent of one another, and if ordered entirely after the Principal
+        ///   Tables there will never be any issues.
+        /// </remarks>
+        /// <param name="translations">
+        ///   The source <see cref="EntityTranslation">translatios</see>.
+        /// </param>
+        /// <returns>
+        ///   The topological order of the Principal Tables of <paramref name="translations"/>.
+        /// </returns>
+        public static IEnumerable<PrincipalTableDef> OrderEntities(IReadOnlyList<EntityTranslation> translations) {
+            // The `indexMap` is effectively our collection of graph vertices. Each vertex is a translation (identified
+            // by its Principal Table); each vertex's index value doesn't actually matter, we just need a way to mark
+            // rows/columns in the adjacency matrix. The Principal Table is used as the key because that's what Foreign
+            // Keys reference.
+            var indexMap = translations
+                .Select((translation, index) => (translation, index))
+                .ToDictionary(
+                    keySelector: pair => pair.translation.Principal.Table,
+                    elementSelector: pair => pair.index
+                );
+
+            // This is our directed graph adjacency matrix. A directed edge from the vertex representing Entity A to the
+            // vertex representing Entity B means that Entity A is referenced by Entity B and must therefore be handled
+            // first; such an edge is indicated by a value of `true` in the matrix. The Translation Layer guarantees
+            // that the graph is acyclic.
+            var adjacencyMatrix = new List<List<bool>>();
+            for (int i = 0; i < translations.Count; ++i) {
+                adjacencyMatrix.Add(Enumerable.Repeat(false, translations.Count).ToList());
+            }
+
+            // We're also going to keep a degree counter; this will make our topological sort algorithm slightly more
+            // efficient.
+            var degrees = Enumerable.Repeat(0, translations.Count).ToList();
+
+            // Now we build up the initial adjacency matrix. Multiple Foreign Keys against the same foreign table only
+            // count for a single edge. We're also guaranteed to have at least one vertex with no outbound edges, since
+            // the graph is guaranteed to be acyclic.
+            var leafs = new Queue<int>();
+            foreach (var translation in translations) {
+                var principalIndex = indexMap[translation.Principal.Table];
+                foreach (var fk in translation.Principal.Table.ForeignKeys) {
+                    var referenceIndex = indexMap[fk.ReferencedTable];
+                    if (!adjacencyMatrix[referenceIndex][principalIndex]) {
+                        ++degrees[principalIndex];
+                        adjacencyMatrix[referenceIndex][principalIndex] = true;
+                    }
+                }
+                if (translation.Principal.Table.ForeignKeys.IsEmpty()) {
+                    leafs.Enqueue(principalIndex);
+                }
+            }
+
+            // With the initial adjacency matrix complete, we now execute a topological ordering. We will use Kahn's
+            // Algorithm to do this; we already have our starting set of vertices (the "leafs"). Note that the resulting
+            // topological order is not necessarily unique, but it is reproducible for a given input ordering.
+            while (!leafs.IsEmpty()) {
+                var referenceIndex = leafs.Dequeue();
+                yield return translations[referenceIndex].Principal;
+
+                for (int principalIndex = 0; principalIndex < translations.Count; ++principalIndex) {
+                    if (adjacencyMatrix[referenceIndex][principalIndex]) {
+                        if (--degrees[principalIndex] == 0) {
+                            leafs.Enqueue(principalIndex);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Kvasir/Transaction/Transactor.cs
@@ -0,0 +1,166 @@
+﻿using Cybele.Extensions;
+using Kvasir.Core;
+using Kvasir.Schema;
+using Kvasir.Translation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Kvasir.Transaction {
+    /// <summary>
+    ///   The Transactor, which bridges the C# entity code with the back-end database based on the translation of those
+    ///   entities and a particular back-end provider/system.
+    /// </summary>
+    internal sealed class Transactor {
+        /// <summary>
+        ///   Constructs a new <see cref="Translator"/> that uses default <see cref="Settings"/>.
+        /// </summary>
+        /// <param name="translations">
+        ///   The set of <see cref="EntityTranslation">translations</see> that define the schema of the back-end
+        ///   database on which the new <see cref="Transactor"/> will operate.
+        /// </param>
+        /// <param name="connection">
+        ///   The database connection through which all commands and queries will be executed.
+        /// </param>
+        /// <param name="commandsFactory">
+        ///   The <see cref="ICommandsFactory"/> with which to create the necessary commands and queries to interact
+        ///   with the tables for <paramref name="translations"/>.
+        /// </param>
+        /// <param name="entityStorage">
+        ///   The functor through which reconstituted Entities will be stored for later look-up.
+        /// </param>
+        public Transactor(
+            IEnumerable<EntityTranslation> translations,
+            IDbConnection connection,
+            ICommandsFactory commandsFactory,
+            Action<object> entityStorage
+        )
+            : this(translations, connection, commandsFactory, entityStorage, Settings.Default) {}
+
+        /// <summary>
+        ///   Constructs a new <see cref="Translator"/> that uses custom <see cref="Settings"/>.
+        /// </summary>
+        /// <param name="translations">
+        ///   The set of <see cref="EntityTranslation">translations</see> that define the schema of the back-end
+        ///   database on which the new <see cref="Transactor"/> will operate.
+        /// </param>
+        /// <param name="connection">
+        ///   The database connection through which all commands and queries will be executed.
+        /// </param>
+        /// <param name="commandsFactory">
+        ///   The <see cref="ICommandsFactory"/> with which to create the necessary commands and queries to interact
+        ///   with the tables for <paramref name="translations"/>.
+        /// </param>
+        /// <param name="entityStorage">
+        ///   The functor through which reconstituted Entities will be stored for later look-up.
+        /// </param>
+        /// <param name="settings">
+        ///   The <see cref="Settings"/> according to which to perform the transaction activity.
+        /// </param>
+        /// <remarks>
+        ///   Note that the settings are not currently used for anything; in fact, there are no traits available in the
+        ///   <see cref="Settings"/> class. Instead, the settings serve as a forward compatibility mechanism that allows
+        ///   us to provide customization of behaviors in the future without necessitating a significant redesign.
+        /// </remarks>
+        public Transactor(
+            IEnumerable<EntityTranslation> translations,
+            IDbConnection connection,
+            ICommandsFactory commandsFactory,
+            Action<object> entityStorage,
+            Settings settings
+        ) {
+            Debug.Assert(translations is not null && !translations.IsEmpty());
+            Debug.Assert(connection is not null && connection.State == ConnectionState.Open);
+            Debug.Assert(commandsFactory is not null);
+            Debug.Assert(entityStorage is not null);
+            Debug.Assert(settings is not null);
+
+            settings_ = settings;
+            entities_ = translations.ToDictionary(t => t.CLRSource, t => t);
+            connection_ = connection;
+            commandsFactory_ = commandsFactory;
+            entityStorage_ = entityStorage;
+
+            var principalCommands = new List<ICommands>();
+            var relationCommands = new List<ICommands>();
+            var tables = new Dictionary<ITable, int>();
+            foreach (var principal in Topology.OrderEntities(translations.ToList())) {
+                tables[principal.Table] = principalCommands.Count;
+                principalCommands.Add(commandsFactory_.CreateCommands(principal.Table));
+
+                // using `Extractor.SourceType` is a little awkward here, but it's the best we have
+                foreach (var relation in entities_[principal.Extractor.SourceType].Relations) {
+                    tables[relation.Table] = entities_.Count + relationCommands.Count;
+                    relationCommands.Add(commandsFactory_.CreateCommands(relation.Table));
+                }
+            }
+
+            // Relation Tables are automatically ordered after Principal Tables; this is guaranteed to be correct, as a
+            // Relation Table can only reference Principal Tables. The relative order of the Relation Tables is not
+            // relevant.
+            commands_ = principalCommands.Concat(relationCommands).ToList();
+            tables_ = tables;
+        }
+
+        /// <summary>
+        ///   Creates each of the constituent Principal and Relation Tables in the back-end database.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        ///   if the transaction creating the necessary tables fails and is successfully rolled back.
+        /// </exception>
+        /// <exception cref="AggregateException">
+        ///   if the transaction creating the necessary tables fails, and the attempt to roll the transaction back also
+        ///   fails.
+        /// </exception>
+        public void CreateTables() {
+            using var transaction = connection_.BeginTransaction();
+            foreach (var command in commands_.Select(c => c.CreateTableCommand)) {
+                command.Connection = connection_;
+                command.Transaction = transaction;
+                command.ExecuteNonQuery();
+            }
+            TryCommitTransaction(transaction);
+        }
+
+        /// <summary>
+        ///   Attempts to commit a <see cref="IDbTransaction">database transaction</see>. If the commit operation fails,
+        ///   an attempt to roll the transaction back is made.
+        /// </summary>
+        /// <param name="transaction">
+        ///   The <see cref="IDbTransaction">database transaction</see>.
+        /// </param>
+        /// <exception cref="InvalidOperationException">
+        ///   if the attempt to commit <paramref name="transaction"/> fails and is successfully rolled back.
+        /// </exception>
+        /// <exception cref="AggregateException">
+        ///   if the attempt to commit <paramref name="transaction"/> fails, and the attempt to roll it back also fails.
+        /// </exception>
+        private static void TryCommitTransaction(IDbTransaction transaction) {
+            try {
+                transaction.Commit();
+            }
+            catch (InvalidOperationException commitEx) {
+                try {
+                    transaction.Rollback();
+                }
+                catch (InvalidOperationException rollbackEx) {
+                    throw new AggregateException(commitEx, rollbackEx);
+                }
+
+                // Re-throw the exception from the failed transaction so that the caller can handle it as they want
+                throw;
+            }
+        }
+
+
+        private readonly Settings settings_;
+        private readonly IReadOnlyList<ICommands> commands_;                // topologically ordered
+        private readonly IReadOnlyDictionary<Type, EntityTranslation> entities_;
+        private readonly IReadOnlyDictionary<ITable, int> tables_;          // maps to index in `commands_` list
+        private readonly IDbConnection connection_;
+        private readonly ICommandsFactory commandsFactory_;
+        private readonly Action<object> entityStorage_;
+    }
+}
