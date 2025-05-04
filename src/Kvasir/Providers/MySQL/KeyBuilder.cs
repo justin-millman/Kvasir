a339c6c46a9885a825c749211215cce2b1b32034
Justin Millman <jsmillman@msn.com>
2025-05-03 23:33:33 -0500

    MySQL Provider: KeyBuilder
    This commit adds a KeyBuilder for MySQL, implementing the IKeyDeclBuilder<T> interface. The builder is relatively
    straightforward, creating a single SQL snippet that is valid for a MySQL provider. It enforces a 64-character limit on
    the length of a key name.

---
+++ src/Kvasir/Providers/MySQL/KeyBuilder.cs
@@ -0,0 +1,59 @@
+﻿using Cybele.Extensions;
+using Kvasir.Schema;
+using Kvasir.Transcription;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Kvasir.Providers.MySQL {
+    /// <summary>
+    ///   An implementation of the <see cref="IKeyDeclBuilder{TDecl}"/> interface for a MySQL provider.
+    /// </summary>
+    internal sealed class KeyBuilder : IKeyDeclBuilder<SqlSnippet> {
+        /// <summary>
+        ///   Constructs a new <see cref="KeyBuilder"/>.
+        /// </summary>
+        public KeyBuilder() {
+            declaration_ = TEMPLATE;
+        }
+
+        /// <inheritdoc/>
+        public void SetName(KeyName name) {
+            Debug.Assert(name is not null);
+            Debug.Assert(declaration_.Contains(NAME_PLACEHOLDER));
+
+            declaration_ = declaration_.Replace(NAME_PLACEHOLDER, $"CONSTRAINT {name.Render()} ");
+        }
+
+        /// <inheritdoc/>
+        public void SetFields(IEnumerable<IField> fields) {
+            Debug.Assert(fields is not null && !fields.IsEmpty());
+            Debug.Assert(declaration_.Contains(FIELDS_PLACEHOLDER));
+
+            declaration_ = declaration_.Replace(FIELDS_PLACEHOLDER, string.Join(", ", fields.Select(f => f.Name.Render())));
+        }
+
+        /// <inheritdoc/>
+        public void SetAsPrimaryKey() {
+            Debug.Assert(declaration_.Contains(TYPE_PLACEHOLDER));
+            declaration_ = declaration_.Replace(TYPE_PLACEHOLDER, "PRIMARY KEY");
+        }
+
+        /// <inheritdoc/>
+        public SqlSnippet Build() {
+            Debug.Assert(!declaration_.Contains(FIELDS_PLACEHOLDER));
+
+            declaration_ = declaration_.Replace(NAME_PLACEHOLDER, "");          // by default, a key has no name
+            declaration_ = declaration_.Replace(TYPE_PLACEHOLDER, "UNIQUE");    // by default, a key is a Candidate Key
+            return new SqlSnippet(declaration_);
+        }
+
+
+        private static readonly string NAME_PLACEHOLDER = "{:0:}";
+        private static readonly string TYPE_PLACEHOLDER = "{:1:}";
+        private static readonly string FIELDS_PLACEHOLDER = "{:2:}";
+        private static readonly string TEMPLATE = $"{NAME_PLACEHOLDER}{TYPE_PLACEHOLDER} ({FIELDS_PLACEHOLDER})";
+
+        private string declaration_;
+    }
+}
---
+++ src/Kvasir/Providers/MySQL/Rendering.cs
@@ -0,0 +1,66 @@
+﻿using Kvasir.Exceptions;
+using Kvasir.Schema;
+using System.Diagnostics;
+
+namespace Kvasir.Providers.MySQL {
+    /// <summary>
+    ///   A collection of extension methods for rendering various identifiers, literals, and expressions for MySQL
+    ///   queries and statements.
+    /// </summary>
+    internal static class Rendering {
+        /// <summary>
+        ///   Renders the name of a key (primary or candidate) for a MySQL DDL.
+        /// </summary>
+        /// <param name="name">
+        ///   The key name.
+        /// </param>
+        /// <returns>
+        ///   A MySQL-compliant rendering of <paramref name="name"/>.
+        /// </returns>
+        /// <exception cref="KvasirException">
+        ///   if the length of <paramref name="name"/> exceeds 64 characters.
+        /// </exception>
+        public static string Render(this KeyName name) {
+            Debug.Assert(name is not null);
+
+            if (name.Length > MAX_KEY_IDENTIFIER_LENGTH) {
+                throw new KvasirException(
+                    "[MYSQL] — " +
+                    $"length of key name '{name}' is {name.Length}, " +
+                    $"which exceeds the maximum of {MAX_KEY_IDENTIFIER_LENGTH} characters"
+                );
+            }
+            return $"{IDENTIFIER_DELIMITER}{name}{IDENTIFIER_DELIMITER}";
+        }
+
+        /// <summary>
+        ///   Renders the name of a Field for a MySQL DDL or query.
+        /// </summary>
+        /// <param name="name">
+        ///   The Field name.
+        /// </param>
+        /// <returns>
+        ///   A MySQL-compliant rendering of <paramref name="name"/>.
+        /// </returns>
+        /// <exception cref="KvasirException">
+        ///   if the length of <paramref name="name"/> exceeds 64 characters.
+        /// </exception>
+        public static string Render(this FieldName name) {
+            Debug.Assert(name is not null);
+
+            if (name.Length > MAX_FIELD_IDENTIFIER_LENGTH) {
+                throw new KvasirException(
+                    "[MYSQL] — " +
+                    $"length of field name '{name}' is {name.Length}, " +
+                    $"which exceeds the maximum of {MAX_FIELD_IDENTIFIER_LENGTH} characters"
+                );
+            }
+            return $"{IDENTIFIER_DELIMITER}{name}{IDENTIFIER_DELIMITER}";
+        }
+
+
+        private static readonly char IDENTIFIER_DELIMITER = '`';
+        private static readonly int MAX_KEY_IDENTIFIER_LENGTH = 64;
+        private static readonly int MAX_FIELD_IDENTIFIER_LENGTH = 64;
+    }
+}
