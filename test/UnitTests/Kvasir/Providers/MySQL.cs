7a52eb3a2cc66e4476f04543d2194acec14d9e18
Justin Millman <jsmillman@msn.com>
2025-05-10 11:27:26 -0500

    MySQL Provider: ForeignKeyBuilder
    This commit adds a ForeignKeyBuilder for MySQL, implementing the IKeyDeclBuilder<T> interaface. The builder is
    relatively straightforward, creating a single SQL snippet that is valid for a MySQL provider. It enforces a 64-character
    limit on the length of a key name. The builder allows 'SET DEFAULT' for on-delete and on-update actions, even though
    these are likely to be rejected by the MySQL engine for several database implementations.

--- test/UnitTests/Kvasir/Providers/MySQL.cs
+++ test/UnitTests/Kvasir/Providers/MySQL.cs
@@ -177,4 +177,396 @@ namespace UT.Kvasir.Providers {
                 .WithMessageContaining("exceeds the maximum of 64 characters");
         }
     }
+
+    [TestClass, TestCategory("MySQL - Foreign Keys")]
+    public class MySqlForeignKeyTests {
+        [TestMethod] public void SingleFieldUnnamed() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Chinquapin"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("OtherTable"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Clanton"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`)"
+            );
+        }
+
+        [TestMethod] public void SingleFieldNamed() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Aleero City"));
+            var keyName = new FKName("FK1");
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("TableBeingReferenced"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Baldur's Gate"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetName(keyName);
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"CONSTRAINT `{keyName}` " +
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`)"
+            );
+        }
+
+        [TestMethod] public void MultipleFieldsUnnamed() {
+            // Arrange
+            var field0 = Substitute.For<IField>();
+            field0.Name.Returns(new FieldName("Amritsar"));
+            var field1 = Substitute.For<IField>();
+            field1.Name.Returns(new FieldName("Rawalpindi"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("TargetTable"));
+            var pkField0 = Substitute.For<IField>();
+            pkField0.Name.Returns(new FieldName("Ciudad Juárez"));
+            var pkField1 = Substitute.For<IField>();
+            pkField1.Name.Returns(new FieldName("Bandung"));
+            var pk = new PrimaryKey(new[] { pkField0, pkField1 });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field0, field1 });
+            builder.SetReferencedTable(table);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field0.Name}`, `{field1.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField0.Name}`, `{pkField1.Name}`)"
+            );
+        }
+
+        [TestMethod] public void MultipleFieldsNamed() {
+            // Arrange
+            var field0 = Substitute.For<IField>();
+            field0.Name.Returns(new FieldName("Tabriz"));
+            var field1 = Substitute.For<IField>();
+            field1.Name.Returns(new FieldName("Varanasi"));
+            var field2 = Substitute.For<IField>();
+            field2.Name.Returns(new FieldName("Rostov-on-Don"));
+            var keyName = new FKName("ForeignKey");
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("Table2"));
+            var pkField0 = Substitute.For<IField>();
+            pkField0.Name.Returns(new FieldName("Gold Coast"));
+            var pkField1 = Substitute.For<IField>();
+            pkField1.Name.Returns(new FieldName("Belém"));
+            var pkField2 = Substitute.For<IField>();
+            pkField2.Name.Returns(new FieldName("Donetsk"));
+            var pk = new PrimaryKey(new[] { pkField0, pkField1, pkField2 });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetName(keyName);
+            builder.SetFields(new[] { field0, field1, field2 });
+            builder.SetReferencedTable(table);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"CONSTRAINT `{keyName}` " +
+                $"FOREIGN KEY (`{field0.Name}`, `{field1.Name}`, `{field2.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField0.Name}`, `{pkField1.Name}`, `{pkField2.Name}`)"
+            );
+        }
+
+        [TestMethod] public void OnActionCascade() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Durban"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("TableNumber2"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Katowice"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnDeleteBehavior(OnDelete.Cascade);
+            builder.SetOnUpdateBehavior(OnUpdate.Cascade);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON DELETE CASCADE " +
+                "ON UPDATE CASCADE"
+            );
+        }
+
+        [TestMethod] public void OnActionPrevent() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Bonn"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("AdjacentTable"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Bhopal"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnDeleteBehavior(OnDelete.Prevent);
+            builder.SetOnUpdateBehavior(OnUpdate.Prevent);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON DELETE RESTRICT " +
+                "ON UPDATE RESTRICT"
+            );
+        }
+
+        [TestMethod] public void OnActionDoNothing() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Nizhny Novgorod"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("NotThisTable"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Almaty"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnDeleteBehavior(OnDelete.NoAction);
+            builder.SetOnUpdateBehavior(OnUpdate.NoAction);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON DELETE NO ACTION " +
+                "ON UPDATE NO ACTION"
+            );
+        }
+
+        [TestMethod] public void OnActionSetDefault() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Goiânia"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("PartnerTable"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Kumasi"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnDeleteBehavior(OnDelete.SetDefault);
+            builder.SetOnUpdateBehavior(OnUpdate.SetDefault);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON DELETE SET DEFAULT " +
+                "ON UPDATE SET DEFAULT"
+            );
+        }
+
+        [TestMethod] public void OnActionSetNull() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Calgary"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("TableTheSecond"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Soweto"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnDeleteBehavior(OnDelete.SetNull);
+            builder.SetOnUpdateBehavior(OnUpdate.SetNull);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON DELETE SET NULL " +
+                "ON UPDATE SET NULL"
+            );
+        }
+
+        [TestMethod] public void OnDeleteDifferentThanOnUpdate() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Bishkek"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("TableNumeroDos"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Tunisia"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnDeleteBehavior(OnDelete.Cascade);
+            builder.SetOnUpdateBehavior(OnUpdate.SetNull);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON DELETE CASCADE " +
+                "ON UPDATE SET NULL"
+            );
+        }
+
+        [TestMethod] public void OnDeleteOnly() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Mysore"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("ThatOtherTableOverThere"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Cuernavaca"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnDeleteBehavior(OnDelete.NoAction);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON DELETE NO ACTION"
+            );
+        }
+
+        [TestMethod] public void OnUpdateOnly() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Gitega"));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("GodTable"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Bulawayo"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            builder.SetOnUpdateBehavior(OnUpdate.Prevent);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`) " +
+                "ON UPDATE RESTRICT"
+            );
+        }
+
+        [TestMethod] public void KeyNameIsMaximumLength() {
+            // Arrange
+            var field = Substitute.For<IField>();
+            field.Name.Returns(new FieldName("Sheffield"));
+            var keyName = new FKName(new string('R', 64));
+
+            var table = Substitute.For<ITable>();
+            table.Name.Returns(new TableName("CousinTable"));
+            var pkField = Substitute.For<IField>();
+            pkField.Name.Returns(new FieldName("Curitiba"));
+            var pk = new PrimaryKey(new[] { pkField });
+            table.PrimaryKey.Returns(pk);
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            builder.SetName(keyName);
+            builder.SetFields(new[] { field });
+            builder.SetReferencedTable(table);
+            var sql = builder.Build();
+
+            // Assert
+            sql.Should().Be(
+                $"CONSTRAINT `{keyName}` " +
+                $"FOREIGN KEY (`{field.Name}`) " +
+                $"REFERENCES `{table.Name}` (`{pkField.Name}`)"
+            );
+        }
+
+        [TestMethod] public void KeyNameExceedsMaximumLength_IsError() {
+            // Arrange
+            var keyName = new FKName(new string(':', 2617));
+
+            // Act
+            var builder = new ForeignKeyBuilder();
+            var action = () => builder.SetName(keyName);
+
+            // Assert
+            action.Should().ThrowExactly<KvasirException>()
+                .WithMessageContaining("[MySQL]")
+                .WithMessageContaining(keyName.ToString())
+                .WithMessageContaining("exceeds the maximum of 64 characters");
+        }
+    }
 }
